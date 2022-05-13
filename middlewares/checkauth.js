a8a08803257b8b5559ff7a95a221c0b988d71751
Andrey Evtushenko <andrevt600@gmail.com>
2022-05-13 17:26:31 +0300

    Authorization checking

--- middlewares/checkauth.js
+++ middlewares/checkauth.js
@@ -1,21 +1,30 @@
 import jwt from 'jsonwebtoken'
+import { validationResult } from 'express-validator'
 import key from '../key.js'
 import HttpError from '../httperror.js'
+import ValidationError from '../errors/validationerror.js'
 
 export default function(req, res, next) {
   try {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      throw new ValidationError(400, 'Wrong fields values', errors.errors)
+    }
+
     const token = req.headers.authorization.split(' ')[1]
     if(!token) {
       throw new HttpError(403, 'Access token not found')
     }
 
-    jwt.verify(token, key)
+    const {email, role} = jwt.verify(token, key)
+    if(email != req.body.email) {
+      if(role != 'admin') {
+        throw new HttpError(403, 'You have no rights for this')
+      }     
+    }
     next()
   } catch(error) {
-    if(error instanceof HttpError) {
-      console.log('HTTP Error:', error.message);
-      res.status(error.code).json({message: error.message})
-    } else if (error instanceof jwt.JsonWebTokenError) {
+    if (error instanceof jwt.JsonWebTokenError) {
       console.log('JWT Error:', error.message);
       res.status(403).json({message: error.message})
     } else {
--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { check } from 'express-validator'
 import controller from './controller.js'
+import checkauth from './middlewares/checkauth.js'
 import auth from './middlewares/checkauth.js'
 import role from './middlewares/checkrole.js'
 import uploadImage from './middlewares/image.js'
@@ -15,14 +16,28 @@ router.post('/registration',
   check('password', 'Password must be at least 4 characters long').isLength({ min: 4 }),
   check('firstName', 'First name can\'t be empty').notEmpty(),
   check('lastName', 'Last name can\'t be empty').notEmpty(),
+  check('role', 'Role can be "user" or "admin"').isIn(['user', 'admin']),
   controller.registration)
-router.post('/login', controller.login)
-router.post('/create', 
+router.post('/login', 
+  check('email', 'Wrong email').exists().isEmail(),
+  check('password', 'Password must be at least 4 characters long').exists().isLength({ min: 4 }),
+  controller.login)
+/* router.post('/create', 
   uploadImage.single('image'),
   check('email', 'Wrong email').isEmail(),
   check('firstName', 'First name can\'t be empty').notEmpty(),
   check('lastName', 'Last name can\'t be empty').notEmpty(),
-  controller.create)
-router.post('/image', uploadImage.single('image'), controller.image)
+  controller.create) */
+router.post('/image',
+  check('Authorization', 'There are no authorization header').exists(),
+  uploadImage.single('image'),
+  check('email', 'Wrong email').isEmail(),
+  checkauth,
+  controller.image)
+router.post('/pdf', 
+  check('Authorization', 'There are no authorization header').exists(),
+  check('email', 'Email is not provided').exists(),
+  checkauth,
+  controller.pdf)
 
 export default router
